ac0a9dc649c8c4807a950c53c6a9cdbfee4dd218
Victor White <victorblanco281201@gmail.com>
2024-08-22 18:08:58 -0400

    Starting to config the backend and the data base

---
+++ README.md
@@ -0,0 +1,15 @@
+# MEAN App
+
+Este repositorio aloja el MEAN App del curso de Angular de Fernando Herrera
+
+## Instalación y ejecución
+
+Primero es neceasario clonar el repositorio.
+
+### Backend
+
+1. Copiar el ```env.template```, renonmbrarlo a ```.env```, y agregar lo que este necesita
+
+2. Ejecutar ```docker compose up -d```
+
+### Frontend
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@localhost:27017/`,
+    ),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
